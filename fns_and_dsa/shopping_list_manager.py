3a506394e95d327c5a4354567e5ae083af64f883
Hanna Tariku <hannatariku1996@gmail.com>
2025-07-30 21:44:50 +0300

    shopping list manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,5 +1,5 @@
 def display_menu():
-    print("\nShopping List Manager")
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
@@ -7,9 +7,6 @@ def display_menu():
 
 def main():
     shopping_list = []
-
-
-
 
     while True:
         display_menu()
@@ -33,7 +30,7 @@ def main():
         
         elif choice == '3':
             if shopping_list:
-                print("\nYour Shopping List:")
+                print("Your Shopping List:")
                 for i, item in enumerate(shopping_list, 1):
                     print(f"{i}. {item}")
             else:
